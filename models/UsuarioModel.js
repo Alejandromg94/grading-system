08ac50e6354ab76223278295c84844268a6e2432
Alejandromg94 <Alejandromg94@outlook.com>
2025-10-28 09:47:31 -0500

    Elimina la función listarUsuarios y simplifica la carga de usuarios en UsuarioModel.js. Se añade una nueva página de registro y se actualiza el formulario de matrícula para corregir rutas de scripts.

--- models/UsuarioModel.js
+++ models/UsuarioModel.js
@@ -1,36 +1,34 @@
-import { consultarLocalStorage } from "helpers/lcar-storage.js";
+import { consultarLocalStorage } from "/helpers/localstorage.js";
 
-export let Usuarios =
-  consultarLocalStorage("usuarios") ||
-  [
-    //   {
-    //     nombre: "Jaime",
-    //     apellido: "Zapata",
-    //     documento: "12352345",
-    //     correo: "jaime@example.com",
-    //     telefono: "3246720301",
-    //     contrasena: "Contrase12*",
-    //     genero: "masculino",
-    //     hobbies: "musica",
-    //   },
-    //   {
-    //     nombre: "Ana",
-    //     apellido: "García",
-    //     documento: "54321678",
-    //     correo: "ana@example.com",
-    //     telefono: "3109876543",
-    //     contrasena: "P4ssw0rd!",
-    //     genero: "femenino",
-    //     hobbies: "lectura",
-    //   },
-    //   {
-    //     nombre: "Carlos",
-    //     apellido: "Rodriguez",
-    //     documento: "98765432",
-    //     correo: "carlos@example.com",
-    //     telefono: "3001234567",
-    //     contrasena: "S3cur3P@ss*",
-    //     genero: "masculino",
-    //     hobbies: "deportes",
-    //   },
-  ];
+export let Usuarios = consultarLocalStorage("Usuarios") || [
+  {
+    nombre: "Jaime",
+    apellido: "Zapata",
+    documento: "12352345",
+    correo: "jaime@example.com",
+    telefono: "3246720301",
+    contrasena: "Contrase12*",
+    genero: "masculino",
+    hobbies: "musica",
+  },
+  {
+    nombre: "Ana",
+    apellido: "García",
+    documento: "54321678",
+    correo: "ana@example.com",
+    telefono: "3109876543",
+    contrasena: "P4ssw0rd!",
+    genero: "femenino",
+    hobbies: "lectura",
+  },
+  {
+    nombre: "Carlos",
+    apellido: "Rodriguez",
+    documento: "98765432",
+    correo: "carlos@example.com",
+    telefono: "3001234567",
+    contrasena: "S3cur3P@ss*",
+    genero: "masculino",
+    hobbies: "deportes",
+  },
+];
